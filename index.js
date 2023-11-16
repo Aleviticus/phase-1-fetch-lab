90fc0a5c30ef292a1dda52973e0a1c036e73c905
Aaron Levy <aaronlevypc21@gmail.com>
2023-11-15 22:20:00 -0500

    practice

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => {
+    const character =data[2]
+    const fifthBook = data[4]
+    let keys = Object.numberofpages = data[0, 10]
+    console.log(keys)
+    console.log(character)
+    console.log(fifthBook)
+    console.log(data);
+
+    renderBooks(data)
+  })
   // To pass the tests, don't forget to return your fetch!
-  
 }
 
 function renderBooks(books) {
