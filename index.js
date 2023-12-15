de3429eea04604fd804f3c3ed4c44c74057040e5
Aaron Levy <aaronlevypc21@gmail.com>
2023-12-15 15:28:02 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
   fetch("https://anapioficeandfire.com/api/books")
   .then((resp) => resp.json())
   .then((data) => {
-    const character =data[2]
+    const character = data[2]
     const fifthBook = data[4]
     console.log(character)
     console.log(fifthBook.numberOfPages)
